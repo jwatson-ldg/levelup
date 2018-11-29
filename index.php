a6db617488dd60f8cd40bb78d98e82b9af36c2f3
Jake Watson <jake.watson@limitlessdigital.com>
2018-11-29 21:28:33 +0000

    Proof of concept WIP

--- index.php
+++ index.php
@@ -1,13 +1,53 @@
 <?php
 
-/**********
- * Config *
- **********/
+/*****************
+ * START: Config *
+ *****************/
 
-// Enter start date in format DD/MM/YY
+// Enter start date in format DD-MM-YY
 $startDate = '04-11-2018';
 
-$introMsg .= "\n\t********************************\n";
+// Previous average mileage for a non-racing training week
+$avgMileage = 60;
+
+// Races during this training block
+$races = [
+    1 => [
+        'date' => '04-11-2018',
+        'type' => 'road',
+        'distance' => 10,
+        'distance_unit' => 'kilometres',
+        'target_race' => true
+    ],
+    2 => [
+        'date' => '25-11-2018',
+        'type' => 'road',
+        'distance' => 10,
+        'distance_unit' => 'kilometres',
+        'target_race' => false
+    ],
+    3 => [
+        'date' => '01-12-2018',
+        'type' => 'road',
+        'distance' => 5,
+        'distance_unit' => 'kilometres',
+        'target_race' => false
+    ]
+];
+
+// Training sessions available during this training block
+$trainingSessions = [
+    1 => [],
+    2 => [],
+    3 => [],
+    4 => []
+];
+
+/***************
+ * END: Config *
+ ***************/
+
+$introMsg = "\n\t********************************\n";
 $introMsg .= "\tLevelup! Training plan generator\n";
 $introMsg .= "\t********************************\n\n";
 

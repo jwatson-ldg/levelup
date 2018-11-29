b4628d7cc2587b58c9877d371d0f9aef810b3ab8
Jake Watson <jake.watson@limitlessdigital.com>
2018-11-29 21:45:08 +0000

    Proof of concept WIP

--- levelup.php
+++ levelup.php
@@ -16,31 +16,111 @@ $races = [
         'date' => '04-11-2018',
         'type' => 'road',
         'distance' => 10,
-        'distance_unit' => 'kilometres',
+        'distance_unit' => 'kilometre',
         'target_race' => true
     ],
     2 => [
         'date' => '25-11-2018',
         'type' => 'road',
         'distance' => 10,
-        'distance_unit' => 'kilometres',
+        'distance_unit' => 'kilometre',
         'target_race' => false
     ],
     3 => [
         'date' => '01-12-2018',
         'type' => 'road',
         'distance' => 5,
-        'distance_unit' => 'kilometres',
+        'distance_unit' => 'kilometre',
         'target_race' => false
     ]
 ];
 
 // Training sessions available during this training block
 $trainingSessions = [
-    1 => [],
-    2 => [],
-    3 => [],
-    4 => []
+    1 => [
+        'date' => '06-11-18',
+        'length_warm_up' => 3,
+        'length_cool_down' => 2,
+        'rep_groups' => [
+            1 => [
+                'rep_distance' => 600,
+                'rep_distance_unit' => 'metre',
+                'rep_qty' => 12,
+                'rep_recovery' => 90,
+                'rep_recovery_unit' => 'second'
+            ]
+        ]
+    ],
+    2 => [
+        'date' => '13-11-18',
+        'length_warm_up' => 3,
+        'length_cool_down' => 2,
+        'rep_groups' => [
+            1 => [
+                'rep_distance' => 400,
+                'rep_distance_unit' => 'metre',
+                'rep_qty' => 1,
+                'rep_recovery' => 1,
+                'rep_recovery_unit' => 'minute'
+            ],
+            2 => [
+                'rep_distance' => 800,
+                'rep_distance_unit' => 'metre',
+                'rep_qty' => 2,
+                'rep_recovery' => 2,
+                'rep_recovery_unit' => 'minute'
+            ],
+            3 => [
+                'rep_distance' => 1,
+                'rep_distance_unit' => 'kilometre',
+                'rep_qty' => 3,
+                'rep_recovery' => 2.5,
+                'rep_recovery_unit' => 'minute'
+            ],
+            4 => [
+                'rep_distance' => 800,
+                'rep_distance_unit' => 'metre',
+                'rep_qty' => 2,
+                'rep_recovery' => 2,
+                'rep_recovery_unit' => 'minute'
+            ],
+            5 => [
+                'rep_distance' => 400,
+                'rep_distance_unit' => 'metre',
+                'rep_qty' => 1,
+                'rep_recovery' => 0,
+                'rep_recovery_unit' => 'second'
+            ]
+        ]
+    ],
+    3 => [
+        'date' => '20-11-18',
+        'length_warm_up' => 3,
+        'length_cool_down' => 2,
+        'rep_groups' => [
+            1 => [
+                'rep_distance' => 5,
+                'rep_distance_unit' => 'minute',
+                'rep_qty' => 4,
+                'rep_recovery' => 2,
+                'rep_recovery_unit' => 'minute'
+            ]
+        ]
+    ],
+    4 => [
+        'date' => '27-11-18',
+        'length_warm_up' => 3,
+        'length_cool_down' => 2,
+        'rep_groups' => [
+            1 => [
+                'rep_distance' => 800,
+                'rep_distance_unit' => 'metre',
+                'rep_qty' => 8,
+                'rep_recovery' => 2,
+                'rep_recovery_unit' => 'minute'
+            ]
+        ]
+    ]
 ];
 
 /***************
